63b887ca2987c32728c5add67360928c5fe148aa
kingkazma1109 <901109danny@gmail.com>
2023-02-23 02:07:52 +0800

    Update q_new function

--- queue.c
+++ queue.c
@@ -14,7 +14,10 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *newQueue = malloc(sizeof(struct list_head));
+    if (newQueue)
+        INIT_LIST_HEAD(newQueue);
+    return newQueue;
 }
 
 /* Free all storage used by queue */
