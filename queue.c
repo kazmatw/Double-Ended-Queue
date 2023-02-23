02335ee3fb7d2a97799144f05446629d1ea46f87
kingkazma1109 <901109danny@gmail.com>
2023-02-24 03:08:16 +0800

    Update q_insert_head function

--- queue.c
+++ queue.c
@@ -35,6 +35,17 @@ void q_free(struct list_head *l)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+    element_t *node = malloc(sizeof(element_t));
+    if (!node)
+        return false;
+    node->value = strdup(s);
+    if (!node->value) {
+        q_release_element(node);
+        return false;
+    }
+    list_add(&node->list, head);
     return true;
 }
 
